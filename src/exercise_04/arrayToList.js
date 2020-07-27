e774c26c2dac6c40a09798670ceb668d2843ba76
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-07-27 21:12:09 +0800

    [fengyi_ling]homework:complete exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,20 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined) {
+    throw 'Creating list from undefined array';
+  }
+  if (array === null) {
+    throw 'Creating list from undefined array';
+  }
+  if (array.length === 0) {
+    throw 'Creating list from empty array';
+  }
+
+  let list = null;
+
+  for (let i = array.length - 1; i >= 0 ; i --) {
+    list = {value: array[i], next: list };
+  }
+  return list;
 }
