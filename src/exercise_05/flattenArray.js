268379cba139fe4edad7ab20e27af16568f32913
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-07-27 21:13:38 +0800

    [fengyi_ling]homework:complete exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,26 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined) {
+    throw 'Flatten undefined or null array';
+  }
+  if (array === null) {
+    throw 'Flatten undefined or null array';
+  }
+  if (array.length === 0) {
+    return [];
+  }
+  //递归
+  let retnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      retnArray.push(array[i]);
+    } else {
+      let currentArray = array[i];
+      for (let j = 0; j < currentArray.length; j++) {
+        retnArray.push(currentArray[j]);
+      }
+    }
+  }
+  return retnArray;
 }
